f1424205822914bf31caea09774f86e91cad52de
ckgh4268 <67631704+ckgh4268@users.noreply.github.com>
2020-09-21 17:43:49 +0900

    Create Q94 ClassCar (main).java

---
+++ Q94 ClassCar (main).java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner staIn = new Scanner(System.in);
+
+    
+    System.out.println("day1을 입력하자.");
+    System.out.print("년 : "); int y = stdIn.nextInt();
+    System.out.print("월 : "); int m = stdIn.nextInt();
+    System.out.print("일 : "); int d = stdIn.nextInt();
+
+    Day day1 = new Day(y,m,d);
+
+
+  }
+}
