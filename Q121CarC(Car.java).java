2f61c0a2bf193d62d480053510bb1f8d9952f78a
ckgh4268 <67631704+ckgh4268@users.noreply.github.com>
2020-09-23 17:42:54 +0900

    Create Q121CarC(Car.java).java

---
+++ Q121CarC(Car.java).java
@@ -0,0 +1,56 @@
+public class Car {
+
+private String name;
+private int width;
+private int height;
+private int length;
+
+private double x; //현재 위치 x
+private double y; //현재 위치 y
+
+private double fuel;  // 남은 연료
+private String purchaseDay; 
+
+// 생성자 //
+
+Car(String name, 
+    int width, 
+    int height,
+    int length, 
+    double fuel, 
+    String purchaseDay) {
+    this.name = name; 
+    this.width = width;
+    this.height = height;
+    this.length = length;
+    this.fuel = fuel;
+    x = y = 0.0;
+    this.purchaseDay = purchaseDay;
+}
+public String getName() { return name;}
+public double getX() { return x;}
+public double getY() { return y;}
+public double getFuel() { return fuel;}
+public String getPurchaseDay() { return purchaseDay;}
+
+public void putSpec() {
+  System.out.println("이름:" + name);
+  System.out.println("전폭:" + width + "mm");
+  System.out.println("전고:" + height + "mm");
+  System.out.println("전장:" + length + "mm");
+}
+//이동 x방향 y방향
+
+  public boolean move(double dx, double dy) {
+    double dist = Math.sqrt(dx * dx + dy * dy);
+
+    if (dist > fuel)
+      return false;
+    else {
+      fuel -= dist;
+      x += dx;
+      y += dy;
+      return true;
+    }
+  }
+}
