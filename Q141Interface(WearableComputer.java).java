68ab50c14f737d0e33ce764fb085c94fa25551fa
ckgh4268 <67631704+ckgh4268@users.noreply.github.com>
2020-09-24 13:11:19 +0900

    Create Q141Interface(WearableComputer.java).java

---
+++ Q141Interface(WearableComputer.java).java
@@ -0,0 +1,15 @@
+public class WearableComputer implements Wearable {
+
+  public void puton() {
+    System.out.println("컴퓨터을 착용했습니다.");
+  }
+
+  public void putoff() {
+    System.out.println("컴퓨터을 벗었습니다.");
+  }
+
+  public void reset() {
+    System.out.println("컴퓨터를 재시작했습니다.");
+  }
+
+}
